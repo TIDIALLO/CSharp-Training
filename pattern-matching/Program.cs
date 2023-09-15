d4a3ccd3651a6cd2c189c2b4a12c0e335f481d26
Tidiane Diallo <diallotidiane014@gmail.com>
2023-09-15 10:13:29 +0000

    switch expression

---
+++ pattern-matching/Program.cs
@@ -0,0 +1,6 @@
+﻿using  pattern_matching;
+
+var direction = SwitchExample.Direction.Right;
+Console.WriteLine($"Map view direction is {direction}");
+Console.WriteLine($"Cardinal orientation is {SwitchExample.ToOrientation(direction)}");
+
---
+++ pattern-matching/SwitchExample.cs
@@ -0,0 +1,30 @@
+﻿namespace pattern_matching;
+
+public class SwitchExample
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Right,
+        Left
+    }
+
+    public enum Orientation
+    {
+        North,
+        South,
+        East,
+        West
+    }
+
+    public static Orientation ToOrientation(Direction direction) => direction switch
+    {
+        Direction.Up => Orientation.North,
+        Direction.Right => Orientation.East,
+        Direction.Down => Orientation.South,
+        Direction.Left => Orientation.West,
+        _ => throw new ArgumentOutOfRangeException(nameof(direction), $"Not expected direction value: {direction}"),
+    };
+
+}
